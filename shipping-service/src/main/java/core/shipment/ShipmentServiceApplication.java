189c1741e643c80898dfbbd3ef783d34b1f37796
lukas.casier <lukas.casier@accenture.com>
2018-06-07 22:36:35 +0200

    Changed the Shipment service to get the IP from Amazon

--- shipping-service/src/main/java/core/shipment/ShipmentServiceApplication.java
+++ shipping-service/src/main/java/core/shipment/ShipmentServiceApplication.java
@@ -3,6 +3,12 @@ package core.shipment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +17,17 @@ public class ShipmentServiceApplication {
 	public static void main(String[] args) {
         SpringApplication.run(ShipmentServiceApplication.class, args);
     }
+	
+	@Bean
+	@Profile("aws")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+	    EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+	    AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+	    config.setDataCenterInfo(info);
+	    info.getMetadata().put(AmazonInfo.MetaDataKey.publicHostname.getName(), info.get(AmazonInfo.MetaDataKey.publicIpv4));
+	    config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+	    config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+	    return config;
+	}
 
 }
