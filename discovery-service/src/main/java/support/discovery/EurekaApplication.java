b7293c2abaef48cf107880e92376fbb933770078
lukas.casier <lukas.casier@accenture.com>
2018-09-16 20:09:27 +0200

    Trying out autobind EIP feature of Eureka

--- discovery-service/src/main/java/support/discovery/EurekaApplication.java
+++ discovery-service/src/main/java/support/discovery/EurekaApplication.java
@@ -11,4 +11,17 @@ public class EurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
+	
+	@Bean
+    @Profile("aws")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        info.getMetadata().put(AmazonInfo.MetaDataKey.publicHostname.getName(), info.get(AmazonInfo.MetaDataKey.publicIpv4));
+        config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+        config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+        config.setNonSecurePort(port);
+        config.setDataCenterInfo(info);
+        return config;
+    }
 }
